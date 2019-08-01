1c0794ae62ba9186d1c7380aefea23d31dfbd97a
ResoDev <mresoo99@gmail.com>
2019-08-01 19:49:08 +0200

    Ep 1 - Basics

---
+++ lib/basic_animation_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class AnimationPage extends StatefulWidget {
+  @override
+  _AnimationPageState createState() => _AnimationPageState();
+}
+
+class _AnimationPageState extends State<AnimationPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController animController;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animController = AnimationController(
+      duration: Duration(seconds: 5),
+      vsync: this,
+    );
+
+    final curvedAnimation = CurvedAnimation(
+      parent: animController,
+      curve: Curves.bounceIn,
+      reverseCurve: Curves.easeOut,
+    );
+
+    animation = Tween<double>(
+      begin: 0,
+      end: 2 * math.pi,
+    ).animate(curvedAnimation)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          animController.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          animController.forward();
+        }
+      });
+
+    animController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Transform.rotate(
+        angle: animation.value,
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(30),
+          child: Image.asset(
+            'assets/resocoder.png',
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    animController.dispose();
+    super.dispose();
+  }
+}
